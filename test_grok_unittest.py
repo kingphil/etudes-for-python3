f1fc029e3af23a09acb95ec9a32b54613fb7816c
Phil King <king.phil@gmail.com>
2020-07-04 03:04:22 +0000

    make flake8 happy; no more purple!

--- test_grok_unittest.py
+++ test_grok_unittest.py
@@ -1,16 +1,17 @@
-## python3 -m unittest [-v]
-## python3 -m unittest test_grok_unittest[.py] [-v]
-## python3 -m unittest -k <pattern>
-## python3 -m unittest discover -s <directory>
+# python3 -m unittest [-v]
+# python3 -m unittest test_grok_unittest[.py] [-v]
+# python3 -m unittest -k <pattern>
+# python3 -m unittest discover -s <directory>
 
-## elpy: C-c C-c, then:
-## unittest.main()
+# elpy: C-c C-c, then:
+# unittest.main()
 
 import unittest
 import sys
 
-## assertEqual, assertNotEqual, assertTrue, assertFalse
-## assertRaises, fail
+
+# assertEqual, assertNotEqual, assertTrue, assertFalse
+# assertRaises, fail
 class TestGrokUnitTest(unittest.TestCase):
     def setUp(self):
         self.thingy = PekObj()
@@ -28,15 +29,17 @@ class TestGrokUnitTest(unittest.TestCase):
             self.thingy.echo("quit")
             self.fail('should have raised HoleyMoleyException')
 
+
 def pek_skip_decorator():
     return unittest.skip('')
+
 
 class TestGrokUnitTest_skip_various_ways(unittest.TestCase):
     @unittest.skip('')
     def test_skip(self):
         self.fail('')
 
-    ## also: skipUnless
+    # also: skipUnless
     @unittest.skipIf(sys.platform == 'linux', 'skip if on Linux')
     def test_skipIf(self):
         print('sanity')
@@ -46,7 +49,7 @@ class TestGrokUnitTest_skip_various_ways(unittest.TestCase):
         self.skipTest('')
         self.fail('')
 
-    def test_skip_explicitly(self):
+    def test_skip_explicitly_with_raise(self):
         raise unittest.SkipTest('')
         self.fail('')
 
@@ -62,6 +65,7 @@ class TestGrokUnitTest_skips_from_setUp(unittest.TestCase):
     def test_fails(self):
         self.fail('')
 
+
 class TestGrokUnitTest_skips_from_setUp_via_raise(unittest.TestCase):
     def setUp(self):
         raise unittest.SkipTest('')
@@ -70,15 +74,18 @@ class TestGrokUnitTest_skips_from_setUp_via_raise(unittest.TestCase):
     def test_fails(self):
         self.fail('')
 
+
 @unittest.skip('')
 class TestGrokUnitTest_skip_class(unittest.TestCase):
     def test_skip(self):
         self.fail('')
 
+
 class TestGrokUnitTest_expected(unittest.TestCase):
     @unittest.expectedFailure
     def test_fail(self):
         self.assertTrue(False, "intentional fail")
+
 
 class TestGrokUnitTest_TestSuite(unittest.TestCase):
     def test_basic_functionality(self):
@@ -87,6 +94,7 @@ class TestGrokUnitTest_TestSuite(unittest.TestCase):
         suite.addTest(TestGrokUnitTest('test_basic_functionality'))
         runner.run(suite)
 
+
 class TestGrokUnitTest_subTest(unittest.TestCase):
     def test_subTest(self):
         characters = ['Jyn', 'Galen']
@@ -94,24 +102,26 @@ class TestGrokUnitTest_subTest(unittest.TestCase):
             with self.subTest(label=character):
                 self.assertTrue(character in characters, "subTest test")
 
+
 def legacy_test():
     assert 2 + 2 == 5
-    qwerty()
 
-## "Re-using old test code": needs a clearer example of this
+
+# "Re-using old test code": needs a clearer example of this
 class TestGrokUnitTests_legacy(unittest.TestCase):
     def test_basic_functionality(self):
         legacy_test_obj = unittest.FunctionTestCase(legacy_test)
-        ## unittest.result.TestResult
+        # unittest.result.TestResult
         test_result = legacy_test_obj.run()
         self.assertFalse(test_result.wasSuccessful(), "legacy test (clunky)")
+
 
 class TestGrokUnitTests_trivia(unittest.TestCase):
     def test_assertRaises_alternate_syntax(self):
         thingy = PekObj()
 
-        ## alternate syntax for assertRaise
-        ## self.assertRaises(excClass, callable, *args)
+        # alternate syntax for assertRaise
+        # self.assertRaises(excClass, callable, *args)
         self.assertRaises(HoleyMoleyException, thingy.echo, "quit")
         self.assertRaises(HoleyMoleyException, echo, "quit")
 
@@ -119,29 +129,32 @@ class TestGrokUnitTests_trivia(unittest.TestCase):
         try:
             thingy.echo('pek')
             self.fail("explicit failure (but won't get here")
-        except(HoleyMoleyException, Exception) as e:
+        except(HoleyMoleyException, Exception):
             pass
+
 
 class HoleyMoleyException(Exception):
     pass
 
+
 class PekObj:
     def echo(self, arg):
         return echo(arg)
+
 
 def echo(arg):
     if arg == "quit":
         raise HoleyMoleyException
     return arg
 
-### NOTES
-## less common syntax invocation
-## python3 -m unittest test_grok_unittest.TestGrokUnitTest [-v]
-## python3 -m unittest test_grok_unittest.TestGrokUnitTest.test_basic_functionality [-v]
+# NOTES
+# less common syntax invocation
+# python3 -m unittest test_grok_unittest.TestGrokUnitTest [-v]
+# python3 -m unittest test_grok_unittest.TestGrokUnitTest.test_name [-v]
 
-## note: does not find .py files with dashes!?
-## not a valid identifier for a package or module
-## https://docs.python.org/3/reference/lexical_analysis.html#identifiers
+# note: does not find .py files with dashes!?
+# not a valid identifier for a package or module
+# https://docs.python.org/3/reference/lexical_analysis.html#identifiers
 
-### TODO
-## skip in relation to setUpClass, tearDownClass, setUpModule, tearDownModule
+# TODO
+# skip in relation to setUpClass, tearDownClass, setUpModule, tearDownModule
