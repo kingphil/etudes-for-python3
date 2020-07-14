2a230397707c8574c300720b032ec0057be4a6c2
Phil King <king.phil@gmail.com>
2020-07-14 23:06:15 +0000

    basic tests around decorators

--- test_constructs.py
+++ test_constructs.py
@@ -2,7 +2,7 @@ import unittest
 
 # note: the page numbers refer to "Python Pocket Reference, Mark Lutz, 5th edition"
 
-class TestGenerator(unittest.TestCase):
+class TestGenerators(unittest.TestCase):
     def test_expression_basic(self):
         # generator expressions: p 51
         # note: ~18 quintillion, for IPv6 fans
@@ -37,3 +37,42 @@ class TestGenerator(unittest.TestCase):
         rv = next(gen_func)
         self.assertTrue(rv is not None, "generator function with send")
         self.assertEqual(rv, 256, "generator function with send")
+
+## decorators: p 85
+def pekorator(f):
+    def wrap():
+        accum = []
+        accum.append('pre')
+        accum.append(f())
+        accum.append('post')
+        return accum
+    return wrap
+
+@pekorator
+def pekfunc():
+    return 'during'
+
+## class decorators: p 96
+def pekclassorator(f):
+    ## note: class decorators return the instance (whereas a normal class
+    ##   __init__ must return None)
+    def wrap_init():
+        instance = f()
+        instance.accum.append('pek')
+        return instance
+    return wrap_init
+
+@pekclassorator
+class _PekClass():
+    def __init__(self):
+        self.accum = []
+        ## to be explicit to the comment above
+        return None
+
+class TestDecorators(unittest.TestCase):
+    def test_decorator_basic(self):
+        self.assertEqual(pekfunc(), 'pre during post'.split())
+
+    def test_decorator_class(self):
+        pek = _PekClass()
+        self.assertEqual(pek.accum, ['pek'])
