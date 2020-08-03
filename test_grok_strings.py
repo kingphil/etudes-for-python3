7d9520d9a31b4dfe3ebdfc5089262800f3275ec2
Phil King <king.phil@gmail.com>
2020-08-03 15:15:19 +0000

    strings: formatting expresion

--- test_grok_strings.py
+++ test_grok_strings.py
@@ -4,7 +4,7 @@ import unittest
 
 
 class GrokStrings(unittest.TestCase):
-    # "Table 3" on p 17-18
+    # 'Table 3' on p 17-18
     def test_sequence_operations(self):
         self.assertTrue('a' in 'this is a test')
         self.assertTrue('test' in 'this is a test')
@@ -31,6 +31,22 @@ class GrokStrings(unittest.TestCase):
         self.assertEqual(''.join(pek), 'winston!')
 
         def func(x):
+            self.assertEqual(len(x), 1)
             return x
         pek = list(map(func, 'winston!'))
         self.assertEqual(''.join(pek), 'winston!')
+
+    # p 28-30
+    def test_string_formating_expression(self):
+        self.assertEqual('%s %s %.2f' % (42, 'spam', 1/3.0), '42 spam 0.33')
+        # string format method
+        self.assertEqual('{0} {1} {2:.2f}'.format(
+            42, 'spam', 1/3.0), '42 spam 0.33')
+        # f-string
+        pek = ['42', 'spam', 1/3.0]
+        self.assertEqual(f'{pek[0]} {pek[1]} {pek[2]:.2f}', '42 spam 0.33')
+
+        # more string format expression
+        self.assertEqual('%+.*f' % (4, -1/3.0), '-0.3333')
+        self.assertEqual('%(x)s' % dict(x='winston!'), 'winston!')
+        self.assertEqual('%(x)04d' % dict(x=21), '0021')
